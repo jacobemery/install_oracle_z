d1c451bb406bc77288c26a3a993c5f68d00b5e5c
root <root@zdblab03.wfws.dmz>
2023-04-21 02:56:29 +0000

    more reliable setup scripts

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
-sudo dnf install ansible python3 -y
-ansible-galaxy install -r requirements.yaml
+dnf update -y
+dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+dnf install python39 ansible -y
+ansible-galaxy collection install ansible.posix
+ansible-galaxy collection install community.general
