8173d6596c3610db6ed2067145e4084509cfbfbf
Jacob Emery <jacob.emery@ibm.com>
2023-05-17 13:37:41 -0700

    update with WIP changes
    Signed-off-by: Jacob Emery <jacob.emery@ibm.com>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 dnf update -y
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm --nogpgcheck
-dnf install python39 ansible-core -y
+dnf install python39 ansible-core pexpect -y
 dnf install python3-pip -y
-python3 -m pip install ansible
+python3 -m pip install ansible passlib
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.general
+ansible-playbook 0_connect_hosts.yaml
